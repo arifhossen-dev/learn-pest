105f22b2da7f52deafab5816b83de2c378408ddc
arifhossen <ahak.bsl@gmail.com>
2025-03-19 06:28:23 +0600

    User must be logged in to access the Products data

--- tests/Feature/ProductsTest.php
+++ tests/Feature/ProductsTest.php
@@ -1,24 +1,34 @@
 <?php
 
 use App\Models\Product;
+use App\Models\User;
+
+use function Pest\Laravel\actingAs;
 use function Pest\Laravel\get;
 
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Pagination\LengthAwarePaginator;
  
 test('homepage contains empty table', function () {
-    get('/products')
+    $user = User::factory()->create();
+
+    actingAs($user)
+        ->get('/products')
         ->assertStatus(200)
         ->assertSee(__('No products found'));
 });
  
 test('homepage contains non empty table', function () { 
+    $user = User::factory()->create();
+    
     $product = Product::create([
         'name'  => 'Product 1',
         'price' => 123,
     ]);
  
-    get('/products')
+
+    actingAs($user)
+        ->get('/products')
         ->assertStatus(200)
         ->assertDontSee(__('No products found'))
         ->assertViewHas('products',function(LengthAwarePaginator $collection)use ($product){
@@ -27,10 +37,14 @@ test('homepage contains non empty table', function () {
 });
 
 test('paginate products tble doesnt contain 11th record',function(){
+    $user = User::factory()->create();
+
     $products = Product::factory(11)->create(); 
     $lastProduct = $products->last();
 
-    get('/products')
+
+    actingAs($user)
+        ->get('/products')
         ->assertStatus(200)
         ->assertViewHas('products', function (LengthAwarePaginator $collection) use ($lastProduct) {
             return $collection->doesntContain($lastProduct);
