8c4ceb3a473019559cfd1511bffe3c9efa41713b
arifhossen <ahak.bsl@gmail.com>
2025-03-19 10:57:21 +0600

    Product index and store api create with test

--- tests/Feature/ProductsTest.php
+++ tests/Feature/ProductsTest.php
@@ -129,3 +129,35 @@ test('admin can delete a product',function(){
     $this->assertModelMissing($product);
     $this->assertDatabaseEmpty('products');
 });
+
+test('api returns products list', function () {
+    $product = Product::factory()->create();
+ 
+    $res = $this->getJson('/api/products')
+        ->assertJson([$product->toArray()]);
+ 
+    expect($res->content())
+        ->json()
+        ->toHaveCount(1);
+});
+
+test('api product store successful', function () {
+    $product = [
+        'name' => 'Product 1',
+        'price' => 123
+    ];
+ 
+    $this->postJson('/api/products', $product)
+        ->assertStatus(201)
+        ->assertJson($product);
+});
+
+test('api product invalid store returns error', function () {
+    $product = [
+        'name' => '',
+        'price' => 123
+    ];
+ 
+    $this->postJson('/api/products', $product)
+        ->assertStatus(422);
+});
