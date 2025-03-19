0dfde8ca1276b169c8519673a8e8188774571197
arifhossen <ahak.bsl@gmail.com>
2025-03-19 06:08:34 +0600

    ProductFactory to test pagination

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,11 +3,13 @@
 namespace App\Models;
 
 use App\Services\CurrencyService;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class Product extends Model
 {
+    use HasFactory;
     protected $fillable = [
         'name',
         'price'
